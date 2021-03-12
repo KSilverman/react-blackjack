9e0b5ff4563489da152baf7be233e3288023ba3d
knekvasil <kajnekvasil@gmail.com>
2021-03-12 16:51:33 -0500

    commit

---
+++ react-blackjack/src/components/Deck.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+import '../assets/css/react-blackjack.css';
+
+function getCardFromDeck():
+
+
+class Deck extends React.Component {
+	constructor (props) {
+		super(props);
+	}
+
+	render() {
+		return(
+
+
+
+
+
+		);
+	};
+}
+
+export default Deck;
